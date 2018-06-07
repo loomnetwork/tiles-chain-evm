b979a08d354276c61af96f0ad6a99b15e9c47914
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-06-07 14:50:37 -0300

    Updates to v1.7.1

--- webclient/src/contract_client.js
+++ webclient/src/contract_client.js
@@ -18,13 +18,8 @@ export default class ContractClient {
       'ws://127.0.0.1:9999/queryws',
     )
 
-    client.txMiddleware = [
-      new NonceTxMiddleware(publicKey, client),
-      new SignedTxMiddleware(privateKey)
-    ]
-
     const from = LocalAddress.fromPublicKey(publicKey).toString()
-    const web3 = new Web3(new LoomProvider(client))
+    const web3 = new Web3(new LoomProvider(client, privateKey))
     const ABI = [{"constant":false,"inputs":[{"name":"_tileState","type":"string"}],"name":"SetTileMapState","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"GetTileMapState","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"state","type":"string"}],"name":"OnTileMapStateUpdate","type":"event"}]
 
     const loomContractAddress = await client.getContractAddressAsync('TilesChain')
